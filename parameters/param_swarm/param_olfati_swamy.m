a296ed0668235d4dca591e0e01bdf7d41adcf089
ArchitGupta-IITB <archit.gupta1006@gmail.com>
2023-11-06 14:10:32 +0530

    Implemented Swamy Algorithm V1

---
+++ parameters/param_swarm/param_olfati_swamy.m
@@ -0,0 +1,33 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Olfati Saber parameters
+% Swamy Edition in 2D
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Coefficients for velocity matching
+
+p_swarm.c_vm  = 3;
+
+p_swarm.a     = 1;
+p_swarm.b     = 5;
+p_swarm.c     = (p_swarm.b-p_swarm.a)/(2*sqrt(p_swarm.a*p_swarm.b));
+p_swarm.delta = 0.2;
+p_swarm.k     = 2;
+
+% Velocity of migration - replace the velocity matching and it uses the
+% same gain P.c_cm
+% P.v_migration = [0 4 0]';  
+
+%% Obstacle parameters
+
+p_swarm.r0 = 10;
+p_swarm.lambda = 1;               % (0,1]
+p_swarm.c_pm_obs = 5;
+p_swarm.c_vm_obs = p_swarm.c_vm;
+
+%% Swamy 2D Terms
+p_swarm.is_active_cyl = false;
+p_swarm.is_swamy_active = true;
+p_physics.gravity = 0;
+p_swarm.a_max_swamy = 10;
+p_swarm.kappa_swamy = 1;
+p_swarm.gamma_swamy = 1;
